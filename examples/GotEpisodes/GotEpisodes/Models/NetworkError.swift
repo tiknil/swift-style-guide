a2cca2af4c9cbfae16d6d15e7dae29feebfce62e
Fabio Butti <butti.fabio@tiknil.com>
2017-12-27 11:18:35 +0100

    - Aggiornata sezione Dependency Injection del readme - Aggiornata libreria NetworkError da OnReal

--- examples/GotEpisodes/GotEpisodes/Models/NetworkError.swift
+++ examples/GotEpisodes/GotEpisodes/Models/NetworkError.swift
@@ -12,74 +12,73 @@ enum NetworkError: CustomNSError {
   
   case notConnectedToInternet
   
-  case internationalRoamingOff
-  
-  case notReachedServer
-  
-  case connectionLost
+  case connectionError
   
   case incorrectDataReturned
+  
+  case badRequest
+  
+  /** Array statico che contiene tutti i valori dell'enumerativo per semplificare l'iterazione su tutta la collezione */
+  static let allValues: [NetworkError] = [.unknown, .notConnectedToInternet, .connectionError, .incorrectDataReturned, .badRequest]
   
   internal init(error: NSError) {
     if error.domain == NSURLErrorDomain {
       switch error.code {
-      case NSURLErrorUnknown:
+        
+      case NSURLErrorUnknown, // -1
+           NSURLErrorCancelled, // -999
+           NSURLErrorCallIsActive, // -1019
+           NSURLErrorDataNotAllowed, // -1020
+           NSURLErrorRequestBodyStreamExhausted, // -1021
+           NSURLErrorNoPermissionsToReadFile, // -1102
+           NSURLErrorSecureConnectionFailed, // -1200
+           NSURLErrorServerCertificateHasBadDate, // -1201
+           NSURLErrorServerCertificateUntrusted,  // -1202
+           NSURLErrorServerCertificateHasUnknownRoot, // -1203
+           NSURLErrorServerCertificateNotYetValid,  // -1204
+           NSURLErrorClientCertificateRejected, // -1205
+           NSURLErrorClientCertificateRequired, // -1206
+           NSURLErrorCannotLoadFromNetwork, // - 2000
+           NSURLErrorCannotCreateFile,  // -3000
+           NSURLErrorCannotOpenFile, // -3001
+           NSURLErrorCannotCloseFile, // -3002
+           NSURLErrorCannotWriteToFile, // -3003
+           NSURLErrorCannotRemoveFile, // -3004
+           NSURLErrorCannotMoveFile, // -3005
+           NSURLErrorDownloadDecodingFailedMidStream, // -3006
+           NSURLErrorDownloadDecodingFailedToComplete: // -3007
         self = .unknown
-      case NSURLErrorCancelled:
-        self = .unknown
-      case NSURLErrorBadURL:
-        self = .incorrectDataReturned
-      case NSURLErrorTimedOut:
-        self = .notReachedServer
-      case NSURLErrorUnsupportedURL:
-        self = .incorrectDataReturned
-      case NSURLErrorCannotFindHost, NSURLErrorCannotConnectToHost:
-        self = .notReachedServer
-      case NSURLErrorDataLengthExceedsMaximum:
-        self = .incorrectDataReturned
-      case NSURLErrorNetworkConnectionLost:
-        self = .connectionLost
-      case NSURLErrorDNSLookupFailed:
-        self = .notReachedServer
-      case NSURLErrorHTTPTooManyRedirects:
-        self = .unknown
-      case NSURLErrorResourceUnavailable:
-        self = .incorrectDataReturned
+        
+      case NSURLErrorBadURL, // -1000
+           NSURLErrorUnsupportedURL, // -1002
+           NSURLErrorDataLengthExceedsMaximum, // -1103
+           NSURLErrorResourceUnavailable, // -1008
+           NSURLErrorRedirectToNonExistentLocation, // -1010
+           NSURLErrorBadServerResponse, // -1011
+           NSURLErrorUserCancelledAuthentication, // -1012
+           NSURLErrorUserAuthenticationRequired: // -1013
+        self = .badRequest
+        
+      case NSURLErrorTimedOut, // -1001
+           NSURLErrorCannotFindHost,
+           NSURLErrorCannotConnectToHost,
+           NSURLErrorNetworkConnectionLost,
+           NSURLErrorDNSLookupFailed,
+           NSURLErrorHTTPTooManyRedirects,
+           NSURLErrorInternationalRoamingOff:
+        self = .connectionError
+        
       case NSURLErrorNotConnectedToInternet:
         self = .notConnectedToInternet
-      case NSURLErrorRedirectToNonExistentLocation, NSURLErrorBadServerResponse:
+        
+      case NSURLErrorZeroByteResource,
+           NSURLErrorCannotDecodeRawData,
+           NSURLErrorCannotDecodeContentData,
+           NSURLErrorCannotParseResponse,
+           NSURLErrorFileDoesNotExist,
+           NSURLErrorFileIsDirectory:
         self = .incorrectDataReturned
-      case NSURLErrorUserCancelledAuthentication, NSURLErrorUserAuthenticationRequired:
-        self = .unknown
-      case NSURLErrorZeroByteResource, NSURLErrorCannotDecodeRawData, NSURLErrorCannotDecodeContentData:
-        self = .incorrectDataReturned
-      case NSURLErrorCannotParseResponse:
-        self = .incorrectDataReturned
-      case NSURLErrorInternationalRoamingOff:
-        self = .internationalRoamingOff
-      case NSURLErrorCallIsActive, NSURLErrorDataNotAllowed, NSURLErrorRequestBodyStreamExhausted:
-        self = .unknown
-      case NSURLErrorFileDoesNotExist, NSURLErrorFileIsDirectory:
-        self = .incorrectDataReturned
-      case
-      NSURLErrorNoPermissionsToReadFile,
-      NSURLErrorSecureConnectionFailed,
-      NSURLErrorServerCertificateHasBadDate,
-      NSURLErrorServerCertificateUntrusted,
-      NSURLErrorServerCertificateHasUnknownRoot,
-      NSURLErrorServerCertificateNotYetValid,
-      NSURLErrorClientCertificateRejected,
-      NSURLErrorClientCertificateRequired,
-      NSURLErrorCannotLoadFromNetwork,
-      NSURLErrorCannotCreateFile,
-      NSURLErrorCannotOpenFile,
-      NSURLErrorCannotCloseFile,
-      NSURLErrorCannotWriteToFile,
-      NSURLErrorCannotRemoveFile,
-      NSURLErrorCannotMoveFile,
-      NSURLErrorDownloadDecodingFailedMidStream,
-      NSURLErrorDownloadDecodingFailedToComplete:
-        self = .unknown
+        
       default:
         self = .unknown
       }
@@ -88,4 +87,79 @@ enum NetworkError: CustomNSError {
       self = .unknown
     }
   }
+  
+  
+  /**
+   Funzione utile per i test per ritornare un qualsiasi codice di errore che corrisponda
+   al NetworkError che chiama la funzione
+   */
+  func anyValidRelativeNSError() -> NSError {
+    var array: [Int]
+    
+    switch self {
+    case .unknown:
+      array = [
+        NSURLErrorUnknown,
+        NSURLErrorCancelled,
+        NSURLErrorCallIsActive,
+        NSURLErrorDataNotAllowed,
+        NSURLErrorRequestBodyStreamExhausted,
+        NSURLErrorNoPermissionsToReadFile,
+        NSURLErrorSecureConnectionFailed,
+        NSURLErrorServerCertificateHasBadDate,
+        NSURLErrorServerCertificateUntrusted,
+        NSURLErrorServerCertificateHasUnknownRoot,
+        NSURLErrorServerCertificateNotYetValid,
+        NSURLErrorClientCertificateRejected,
+        NSURLErrorClientCertificateRequired,
+        NSURLErrorCannotLoadFromNetwork,
+        NSURLErrorCannotCreateFile,
+        NSURLErrorCannotOpenFile,
+        NSURLErrorCannotCloseFile,
+        NSURLErrorCannotWriteToFile,
+        NSURLErrorCannotRemoveFile,
+        NSURLErrorCannotMoveFile,
+        NSURLErrorDownloadDecodingFailedMidStream,
+        NSURLErrorDownloadDecodingFailedToComplete]
+      
+    case .badRequest:
+      array = [
+        NSURLErrorBadURL,
+        NSURLErrorUnsupportedURL,
+        NSURLErrorDataLengthExceedsMaximum,
+        NSURLErrorResourceUnavailable,
+        NSURLErrorRedirectToNonExistentLocation,
+        NSURLErrorBadServerResponse,
+        NSURLErrorUserCancelledAuthentication,
+        NSURLErrorUserAuthenticationRequired]
+      
+    case .connectionError:
+      array = [
+        NSURLErrorTimedOut,
+        NSURLErrorCannotFindHost,
+        NSURLErrorCannotConnectToHost,
+        NSURLErrorNetworkConnectionLost,
+        NSURLErrorDNSLookupFailed,
+        NSURLErrorHTTPTooManyRedirects,
+        NSURLErrorInternationalRoamingOff]
+      
+    case .notConnectedToInternet:
+      array = [NSURLErrorNotConnectedToInternet]
+      
+    case .incorrectDataReturned:
+      array = [
+        NSURLErrorZeroByteResource,
+        NSURLErrorCannotDecodeRawData,
+        NSURLErrorCannotDecodeContentData,
+        NSURLErrorCannotParseResponse,
+        NSURLErrorFileDoesNotExist,
+        NSURLErrorFileIsDirectory]
+      
+    }
+    
+    let randomIndex = Int(arc4random_uniform(UInt32(array.count)))
+    
+    return NSError(domain: NSURLErrorDomain, code: array[randomIndex])
+  }
+  
 }
--- examples/GotEpisodes/README.md
+++ examples/GotEpisodes/README.md
@@ -65,30 +65,30 @@ Quando possibile prediligiamo la **Construction injection**, altrimenti utilizzi
 ```Swift
 // Interfaccia della dipendenza
 protocol DependencyProtocol {
-	func awesomeMethod()
+  func awesomeMethod()
 }
 
 // Implementazione della dipendenza
 class Dependency: DependencyProtocol {
-	public func awesomeMethod() {
-		print("Awesome method")
-	}
+  public func awesomeMethod() {
+    print("Awesome method")
+  }
 }
 
 // Classe client con dipendenza da interfaccia
 class Client {
-	public let dependency: DependencyProtocol
+  public let dependency: DependencyProtocol
 	
-	init(dependency: DependencyProtocol) {
-		self.dependency = dependency
-	}
+  init(dependency: DependencyProtocol) {
+    self.dependency = dependency
+  }
 }
 
 // Classe STUB utilizzata nei test
 class StubDependency: DependencyProtocol {
-	public func awesomeMethod() {
-		print("STUBBED awesome method")
-	}
+  public func awesomeMethod() {
+    print("STUBBED awesome method")
+  }
 } 
 ```
 
@@ -96,18 +96,18 @@ class StubDependency: DependencyProtocol {
 ```Swift
 // Classe della dipendenza
 class Dependency {
-	public func awesomeMethod() {
-		print("Awesome method")
-	}
+  public func awesomeMethod() {
+    print("Awesome method")
+  }
 }
 
 // Classe client con dipendenza da classe
 class Client {
-	public let dependency: Dependency
-	
-	init(dependency: Dependency) {
-		self.dependency = dependency
-	}
+  public let dependency: Dependency
+  
+  init(dependency: Dependency) {
+    self.dependency = dependency
+  }
 }
 
 // Nei test è scomodo sostituire la dipendenza
@@ -121,6 +121,66 @@ Un **IoC Container** è un oggetto *singleton* che offre le seguenti funzionalit
 * **Risoluzione** di classi o interfacce: l'operazione di risoluzione (*resolve*) ritorna un'istanza della classe/interfaccia in oggetto.
 
 Queste semplici operazioni permettono di fornire un metodo unico per la creazione di istanze di classi/interfacce. Se tali istanze sono utilizzate come dipendenze è sufficiente cambiare la *registrazione* di tali dipendenze per modificare la dipendenza nell'intera applicazione.
+
+### Risoluzione
+
+#### Semplice
+
+```Swift
+// Risoluzione di un oggetto che implementa il protocollo MyAwesomeProtocol.
+// La classe dell'oggetto dipende da come è stato registrato nel container (vedi paragrafo successivo).
+let objectFromProtocol = container.resolve(MyAwesomeProtocol.self)
+
+// Risoluzione di un oggetto della classe di tipo MyAwesomeClass.
+let objectFromClass = container.resolve(MyAwesomeClass.self)
+```
+
+#### Parametrizzata
+
+Se una classe/protocollo è stata registrata con 1 o più parametri la risoluzione può essere fatta nel modo seguente:
+
+```Swift
+// 1 parametro
+let objectWithOneParam = container.resolve(MyAwesomeProtocol.self, argument: param1)
+
+// 2 parametri
+let objectWithTwoParams = container.resolve(MyAwesomeClass.self, arguments: param1, param2)
+```
+
+### Registrazione
+
+#### Contratta
+
+```Swift
+// Registrazione della classe MyAwesomeClass come implementazione del protocollo MyAwesomeProtocol.
+// In fase di risoluzione del protocollo MyAwesomeProtocol verrà quindi istanziata la class MyAwesomeClass;
+// nei test si può effettuare un'override in modo che in fase di risoluzione venga istanziato uno stub.
+container.register(MyAwesomeProtocol.self) { _ in
+  MyAwesomeClass()
+}
+```
+
+#### Estesa
+
+```Swift
+// Il parametro r è un riferimento al container (resolver) e permette quindi di risolvere altri oggetti.
+// In questo caso viene utilizzato per effettuare una dependency injection tramite setter.
+container.register(MyAwesomeProtocol.self) { r in
+  let myAwesomeObject = MyAwesomeClass()
+  myAwesomeObject.dependency = r.resolve(DependencyProtocol.self)
+  return myAwesomeObject
+}
+```
+
+#### Parametrizzata
+
+```Swift
+// Param può essere di qualsiasi tipo e la risoluzione funzionerà solo se viene
+// passato esattamente quel tipo, altrimenti Swinject lancia un'eccezione.
+container.register(MyAwesomeProtocol.self) { (r: Resolver, param: DependencyProtocol) in
+  MyAwesomeClass(dependency: param) // Dependency injection tramite costruttore
+}
+```
 
 Nell'[AppDelegate](https://github.com/tiknil/swift-style-guide/blob/master/examples/GotEpisodes/GotEpisodes/Application/AppDelegate.swift) è possibile vedere degli esempi di setup del **container** tramite **Swinject**.
 
